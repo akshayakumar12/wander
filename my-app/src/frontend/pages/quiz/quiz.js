3cb98a3b8dfa74afe80a104c522f9d198f9cdef1
shrshre <82681298+shrshre@users.noreply.github.com>
2022-10-02 17:02:58 -0400

    Quiz page skeleton

---
+++ my-app/src/frontend/pages/quiz/quiz.js
@@ -0,0 +1,13 @@
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+function quiz() {
+    return (
+        <h1>This is the quiz</h1>
+        
+    );
+}
+
+export default quiz;
