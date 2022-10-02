3ecf4a2281de6c55821deadb218843e15828058b
shrshre <82681298+shrshre@users.noreply.github.com>
2022-10-02 17:54:40 -0400

    Quiz skeleton pt.2

--- my-app/src/frontend/pages/quiz/quiz.js
+++ my-app/src/frontend/pages/quiz/quiz.js
@@ -3,9 +3,39 @@ import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+
+
 function quiz() {
+
+    // Questions of the quiz
+    // Find a way to make this more robust later :')
+    const questions = [
+		{
+			questionText: 'What genre of music do you like?',
+			answerOptions: [
+				{ answerText: 'Pop'},
+				{ answerText: 'Rap'},
+				{ answerText: 'R&B'},
+				{ answerText: 'Country'},
+			],
+		},
+	];
+
     return (
-        <h1>This is the quiz</h1>
+        <>
+        <h1>quiz</h1>
+
+        <div className='question-text'>{questions[0].questionText}</div>
+
+        <div className='answer-section'>
+	        {questions[0].answerOptions.map((answerOption, index) => (
+		        <>
+                <button>{answerOption.answerText}</button>
+                <br></br>
+                </>
+	        ))}
+        </div>
+        </>
         
     );
 }
