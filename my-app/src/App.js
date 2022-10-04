6b118b00d815d63e8ffa99529e7e64309ac94405
akshayakumar12 <kumar455@purdue.edu>
2022-10-03 21:58:32 -0400

    error handling with login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Login from "./frontend/pages/login/login";
-import Register from "./frontend/pages/register/register"
 
 function App() {
   return <div className="App">
--- my-app/src/backend/pages/login/login.js
+++ my-app/src/backend/pages/login/login.js
@@ -6,7 +6,15 @@ async function login(email, password) {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
         alert("Logged In!");
     } catch (error) {
-        alert(error.message);
+        if (error.code === "auth/invalid-email") {
+            alert("Your email is invalid.");
+        } else if (error.code === "auth/user-not-found") {
+            alert("Your username is invalid.");
+        } else if (error.code === "auth/wrong-password") {
+            alert("Your password is incorrect.");
+        } else if (error.code === "auth/user-disabled") {
+            alert("Your account has been disabled.");
+        } 
     }
 };
 
