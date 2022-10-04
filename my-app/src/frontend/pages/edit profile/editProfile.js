1795030a0e31f148c7634b0a0cc339b928c11225
visathongdee <tthongde@purdue.edu>
2022-10-04 14:15:49 -0400

    Created a skeleton UI for edit profile page

---
+++ my-app/src/frontend/pages/edit profile/editProfile.js
@@ -0,0 +1,64 @@
+import Header from '../header/header'
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export default function editProfile() {
+    return (
+
+        <Box>
+            {/* Header */}
+            <Header />
+
+            {/* My Profile Title */}
+            <Stack alignItems={"flex-start"} style={{marginLeft: "50px", marginRight: "50px", }}><h1>Edit Profile</h1></Stack>
+
+            {/* Components Stack */}
+            <Stack direction="row" alignItems="center" justifyContent="space-between" style={{padding: "0px", marginLeft: "50px", marginRight: "50px", }}>
+
+                
+                {/* Profile Picture*/}
+                <Stack spacing={2} alignItems="center" width="25%">
+                    <Avatar 
+                        src="/broken-image.jpg" 
+                        sx={{ width: 150, height: 150}}
+                    />
+                    <Button variant="contained" disableElevation uppercase={false}>Upload new photo</Button>
+                </Stack>
+
+                {/* Text Fields */}
+                <Stack direction="column" justifyContent="center" alignItems="stretch" spacing={2} width="70%">
+                    <TextField
+                        label="Full Name"
+                        defaultValue="First "
+                    />
+                    <TextField
+                        label="Username"
+                        defaultValue="username"
+                    />
+                    <TextField
+                        label="Email Address"
+                        defaultValue="name@email.com"
+                    />
+                    <TextField
+                        label="Old Password"
+                        type="password"
+                    />
+                    <TextField
+                        label="New Password"
+                        type="password"
+                    />
+                    <TextField
+                        label="Confirm Password"
+                        type="password"
+                    />
+
+                    <Button variant="contained" disableElevation uppercase={false} margin-right="50px">Submit</Button>
+                </Stack>
+            </Stack>
+        </Box>
+
+    )
+}
