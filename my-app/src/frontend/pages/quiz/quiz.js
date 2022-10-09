017ca7fac6769000512dd12cbae92eac36060e7c
shrshre <82681298+shrshre@users.noreply.github.com>
2022-10-09 18:16:32 -0400

    Multiple questions and question navigation in quiz

--- my-app/src/frontend/pages/quiz/quiz.js
+++ my-app/src/frontend/pages/quiz/quiz.js
@@ -1,9 +1,7 @@
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import React, { useState } from 'react';
 
-function quiz() {
+function Quiz() {
 
     // Questions of the quiz
     // Find a way to make this more robust later :')
@@ -17,25 +15,92 @@ function quiz() {
 				{ answerText: 'Country'},
 			],
 		},
+        {
+            questionText: "What's the mood of your trip?",
+			answerOptions: [
+				{ answerText: 'Happy'},
+				{ answerText: 'Romantic'},
+				{ answerText: 'Sad'},
+				{ answerText: 'Angry'},
+			],
+        },
+        {
+            questionText: "What is your favorite color",
+			answerOptions: [
+				{ answerText: 'Red'},
+				{ answerText: 'Orange'},
+				{ answerText: 'Yellow'},
+				{ answerText: 'Green'},
+                { answerText: 'Blue'},
+                { answerText: 'Purple'},
+                { answerText: 'Cyan'},
+                { answerText: 'Fuchsia'},
+			],
+        }
 	];
 
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showEnd, setShowEnd] = useState(false);
+
+    const [flag, setFlag] = React.useState(true); // Change color of selected question
+    const handleAnsClick = () => { 
+        setFlag(!flag);
+        if (currentQuestion < questions.length - 1) {
+            const nextQuestion = currentQuestion + 1;
+            setCurrentQuestion(nextQuestion);
+
+            // firebase stuff?
+
+        }
+        else if (currentQuestion == questions.length - 1) {
+            setShowEnd(true);
+        }
+    };
+
+
+
+    const handlePrevButtonClick = (answerOption) => { // Decrement question when Prev Button is pressed
+        if (currentQuestion > 0) {
+            const prevQuestion = currentQuestion - 1;
+            setCurrentQuestion(prevQuestion);
+            if (showEnd) {
+                setShowEnd(false);
+                setCurrentQuestion(0);
+            }
+        }
+    };
+
     return (
-        <>
-        <h1>quiz</h1>
-
-        <div className='question-text'>{questions[0].questionText}</div>
-
-        <div className='answer-section'>
-	        {questions[0].answerOptions.map((answerOption, index) => (
-		        <>
-                <Button variant="outlined" sx={{ width: 200, padding: 1, margin: 2 }}>{answerOption.answerText}</Button>
+        <div className='app'>
+        {showEnd ? (
+            <div className='end-section'>
+                <h1>quiz</h1>
+                You have reached the end of the quiz
                 <br></br>
+                <Button variant="contained" sx={{ width: 200, padding: 2, margin: 2 }} onClick={() => handlePrevButtonClick()}> {"Start Over"}</Button>
+            </div>
+        ) : (
+                <>
+                    <h1>quiz</h1>
+
+                    <div className='question-text'>{questions[currentQuestion].questionText}</div>
+
+                    <div className='answer-section'>
+                        {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+                            <>
+                            <Button variant="outlined" sx={{ width: 200, padding: 1, margin: 2 }} 
+                                onClick={handleAnsClick}>
+                            {answerOption.answerText}</Button>
+                            <br></br>
+                            </>
+                        ))}
+                    </div>
+                    <Button variant="contained" sx={{ width: 100, padding: 1, margin: 1 }} onClick={() => handlePrevButtonClick()}> {"Prev"}</Button>
                 </>
-	        ))}
+        	)}
         </div>
-        </>
         
     );
 }
 
-export default quiz;
+export default Quiz;
