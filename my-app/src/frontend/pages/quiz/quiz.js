030113da483f0acefc19eaf2ec51d913b44ae280
shrshre <82681298+shrshre@users.noreply.github.com>
2022-10-11 01:46:10 -0400

    Quiz answers saved as an array

--- my-app/src/frontend/pages/quiz/quiz.js
+++ my-app/src/frontend/pages/quiz/quiz.js
@@ -36,36 +36,70 @@ function Quiz() {
                 { answerText: 'Cyan'},
                 { answerText: 'Fuchsia'},
 			],
+        },
+        {
+            questionText: "Who is your favorite U.S. President in the last 50 years?",
+            answerOptions: [
+                { answerText: 'Joe Biden'},
+                { answerText: 'Donald Trump'},
+                { answerText: 'Barack Obama'},
+                { answerText: 'George Bush'},
+                { answerText: 'Bill Clinton'},
+                { answerText: 'George Bush Sr.'},
+                { answerText: 'Ronald Reagan'},
+                { answerText: 'Jimmy Carter'},
+                { answerText: 'Gerald Ford'},
+                { answerText: 'Richard Nixon'},      
+            ],
         }
+
 	];
 
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [showEnd, setShowEnd] = useState(false);
+    const [answers, setAnswers] = useState([]);
+
+    const ansArr = ["hi"]
+
 
     const [flag, setFlag] = React.useState(true); // Change color of selected question
-    const handleAnsClick = () => { 
+    const handleAnsClick = (answerOption) => { 
         setFlag(!flag);
-        if (currentQuestion < questions.length - 1) {
+        if (currentQuestion == questions.length - 1) {
+            
+            /*console.log(answerOption)
+            ansArr = ansArr.concat(answerOption)
+            console.log(ansArr)*/
+            answers.push(answerOption)
+            console.log(answers)
+            
+            setShowEnd(true);
+        }
+        else if (currentQuestion < questions.length - 1) {
             const nextQuestion = currentQuestion + 1;
             setCurrentQuestion(nextQuestion);
 
-            // firebase stuff?
-
+            answers.push(answerOption)
+            console.log(answers)
+            
+            /*console.log(answerOption)
+            ansArr = ansArr.concat(answerOption)
+            console.log(ansArr)*/
         }
-        else if (currentQuestion == questions.length - 1) {
-            setShowEnd(true);
-        }
+        
     };
-
-
 
     const handlePrevButtonClick = (answerOption) => { // Decrement question when Prev Button is pressed
         if (currentQuestion > 0) {
+            answers.pop()
             const prevQuestion = currentQuestion - 1;
             setCurrentQuestion(prevQuestion);
             if (showEnd) {
-                setShowEnd(false);
-                setCurrentQuestion(0);
+                while (answers.length > 0) { // remove all answers 
+                    answers.pop()
+                }
+                setShowEnd(false)
+                setCurrentQuestion(0)
             }
         }
     };
@@ -76,8 +110,10 @@ function Quiz() {
             <div className='end-section'>
                 <h1>quiz</h1>
                 You have reached the end of the quiz
+
                 <br></br>
                 <Button variant="contained" sx={{ width: 200, padding: 2, margin: 2 }} onClick={() => handlePrevButtonClick()}> {"Start Over"}</Button>
+                <Button variant="contained" sx={{ width: 200, padding: 2, margin: 2 }}> {"Submit Quiz"}</Button>
             </div>
         ) : (
                 <>
@@ -89,7 +125,7 @@ function Quiz() {
                         {questions[currentQuestion].answerOptions.map((answerOption, index) => (
                             <>
                             <Button variant="outlined" sx={{ width: 200, padding: 1, margin: 2 }} 
-                                onClick={handleAnsClick}>
+                                onClick={() => handleAnsClick(answerOption.answerText)}>
                             {answerOption.answerText}</Button>
                             <br></br>
                             </>
