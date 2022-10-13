222d2984812706f75434de8c9c93c2983ab44796
shrshre <82681298+shrshre@users.noreply.github.com>
2022-10-13 18:42:52 -0400

    change addDoc to setDoc

--- my-app/src/backend/pages/quiz/quizSend.js
+++ my-app/src/backend/pages/quiz/quizSend.js
@@ -1,4 +1,4 @@
-import { setDoc, addDoc, collection } from 'firebase/firestore/lite';
+import { doc, setDoc, addDoc, collection } from 'firebase/firestore/lite';
 import { auth, firestore } from '../../../firebase';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import {useEffect, useState} from 'react';
@@ -8,11 +8,20 @@ import {useNavigate } from 'react-router-dom';
 
 async function QuizSend(email, answers) {
 
+    const docRef = doc(firestore, "quizAnswers", email);
+    const data = {
+        quiz_id: email,
+        quiz_ans: answers,
+    }
+
+    await setDoc(docRef, data);
+
+    /*
     await addDoc(collection(firestore, "quizAnswers"), 
     {
         quiz_id: email,
         quiz_ans: answers,
-    });
+    });*/
 }
 
 export default QuizSend;
