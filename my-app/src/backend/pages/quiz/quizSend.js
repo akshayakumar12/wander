b340fa661daee262f4616f1bd571c882c43a0e78
shrshre <82681298+shrshre@users.noreply.github.com>
2022-10-13 19:41:27 -0400

    Fixed queries of music preferences

--- my-app/src/backend/pages/quiz/quizSend.js
+++ my-app/src/backend/pages/quiz/quizSend.js
@@ -1,18 +1,21 @@
-import { setDoc, addDoc, collection } from 'firebase/firestore/lite';
+import { doc, setDoc, addDoc, collection } from 'firebase/firestore/lite';
 import { auth, firestore } from '../../../firebase';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import {useEffect, useState} from 'react';
-import { db } from "../../../firebase"
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import {useNavigate } from 'react-router-dom';
 
 async function QuizSend(email, answers) {
 
-    await addDoc(collection(firestore, "quizAnswers"), 
+    const docRef = doc(firestore, "quizAnswers", email);
+    const data = {
+        quiz_id: email,
+        quiz_ans: answers,
+    }
+
+    await setDoc(docRef, data);
+
+    /*await addDoc(collection(firestore, "quizAnswers"), 
     {
         quiz_id: email,
         quiz_ans: answers,
-    });
+    });*/
 }
 
 export default QuizSend;
