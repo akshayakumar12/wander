a11dea338f8a074d0b062f22ab986058954024a2
akshayakumar12 <kumar455@purdue.edu>
2022-10-13 22:59:54 -0400

    upload profile picture

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,8 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import firebase from 'firebase/compat/app'
 import "firebase/compat/firestore"
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -22,3 +23,4 @@ export const firestore = getFirestore(app);
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
+export const storage = getStorage(app);
--- my-app/src/frontend/pages/edit profile/editProfile.js
+++ my-app/src/frontend/pages/edit profile/editProfile.js
@@ -9,10 +9,10 @@ import DialogActions from '@mui/material/DialogActions';
 import * as React from 'react'
 import { DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import { useState, useEffect } from 'react'; 
-import {auth, db, record} from "../../../firebase"
+import { auth, db, record, storage } from "../../../firebase"
 import { onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import { getDatabase, ref, set } from "firebase/database";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export default function EditProfile() {
     const [open, setOpen] = React.useState(false);
@@ -65,6 +65,32 @@ export default function EditProfile() {
         })
     }, [])
 
+
+    // PROFILE PICTURE FUNCTIONALITY
+    const [image, setImage] = useState(null);
+    const [url, setURL] = useState(null);
+
+    const handleImageChange = (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    }
+
+    const handleUpload = () => {
+        const imageRef = ref(storage, auth.currentUser.uid + ".jpg");
+        uploadBytes(imageRef, image).then(() => {
+            getDownloadURL(imageRef).then((url) => {
+                setURL(url);
+            }).catch(error => {
+                console.log(error.message, "error getting the image url");
+            })
+            setImage(null);
+        }).catch(error => {
+            console.log(error.message);
+        });
+    }
+
+
     const [first, setFirst] = React.useState("");
     const [f, setF] = React.useState("abcd");
     
@@ -89,13 +115,14 @@ export default function EditProfile() {
                 {/* Profile Picture*/}
                 <Stack spacing={2} alignItems="center" width="25%">
                     <Avatar 
-                        src="/broken-image.jpg"
+                        src={url}
                         sx={{ width: 150, height: 150}}
                     />
-                    <input type = "file" />
+                    <input type = "file" onChange={handleImageChange}/>
                     <Button 
                         variant="contained" 
-                        disableElevation uppercase={false}>
+                        disableElevation uppercase={false}
+                        onClick={handleUpload}>
                             Upload new photo
                     </Button>
                 </Stack>
