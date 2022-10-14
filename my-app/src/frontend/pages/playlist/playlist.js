fe341bb9fc1dc92f1a31c9eec70b56f7df840717
goodvid <vuppalav@purdue.edu>
2022-10-13 21:54:26 -0400

    Merge branch 'maplin' playlist

--- my-app/src/frontend/pages/playlist/playlist.js
+++ my-app/src/frontend/pages/playlist/playlist.js
@@ -1,26 +1,14 @@
-import Button from "@mui/material/Button";
-
+import React from "react";
 function Playlist() {
   return (
     <div>
       <h1>Playlist for your current trip</h1>
       <body>
         <iframe
-          style="border-radius:12px"
-          src="https://open.spotify.com/embed/playlist/37i9dQZF1DXca8AyWK6Y7g?utm_source=generator"
-          width="100%"
-          height="380"
-          frameBorder="0"
-          allowfullscreen=""
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
+          title="react"
+          src="https://open.spotify.com/embed/playlist/37i9dQZF1EQqkOPvHGajmW?utm_source=generator"
         ></iframe>
       </body>
-      <Button>Edit playlist</Button>
-      <h3>Other information for this trip</h3>
-      <p>Date</p>
-      <p>Starting point</p>
-      <p>Ending point</p>
     </div>
   );
 }
