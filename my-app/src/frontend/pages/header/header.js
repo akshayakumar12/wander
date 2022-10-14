16c38d2553d2b0157b2bfa9e266b7680583c606f
visathongdee <tthongde@purdue.edu>
2022-10-14 02:02:14 -0400

    Made a drop down menu in header for page navi

--- my-app/src/frontend/pages/header/header.js
+++ my-app/src/frontend/pages/header/header.js
@@ -2,13 +2,14 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+//import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 //import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
+//import Switch from '@mui/material/Switch';
+//import FormControlLabel from '@mui/material/FormControlLabel';
+//import FormGroup from '@mui/material/FormGroup';
+import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 //import Menu from '@mui/material/Menu';
 import Logo from '../wander logo.png'
@@ -16,49 +17,98 @@ import Avatar from '@mui/material/Avatar';
 import {useEffect, useState} from 'react';
 import { auth, db } from "../../../firebase"
 import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const pages = ['Home', 'Past Trips', 'Profile', 'Settings'];
 
 function Header() {
     const [userInfo, setUserInfo] = useState("");    
 
-const getData = async () => {
-    const response = db.collection('users');
-    const data = await response.get();
-    data.docs.forEach(item=>{
-        if (item.data().email == auth.currentUser.email) {
-            setUserInfo(item.data())
-        }
-    })
-}
-    
-useEffect(()=> {
-    onAuthStateChanged(auth, () => {
-        getData();
-    })
-}, []) 
+    const getData = async () => {
+        const response = db.collection('users');
+        const data = await response.get();
+        data.docs.forEach(item=>{
+            if (item.data().email === auth.currentUser.email) {
+                setUserInfo(item.data())
+            }
+        })
+    }
+        
+    useEffect(()=> {
+        onAuthStateChanged(auth, () => {
+            getData();
+        })
+    }, []) 
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    function handleClose() {
+        setAnchorEl(null);
+    }
+
+    //const navigate = useNavigate(); 
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" style={{background: '#f6eae2'}}>
-            <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-            >
-                <MenuIcon sx={{color: '#023a7e'}} />
-            </IconButton>
-            <img src={Logo} alt="Brand Logo" height={75} />
+            <AppBar position="static" style={{background: '#f6eae2'}}>
+                <Toolbar>
 
-            <Avatar 
-                src={userInfo?.profilePicture}
-                sx={{ marginLeft: "auto" }}
-            />
-            </Toolbar>
-        </AppBar>
+                    {/* Three Bar Menu Icon */}
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        <MenuIcon sx={{color: '#023a7e'}} />
+                    </IconButton>
+                    {/* Dropdown Menu */}
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        {/*
+                        <MenuItem 
+                            onClick={() => {
+                                navigate("/home");
+                                handleClose();
+                            }}>
+                            Home
+                        </MenuItem>
+                        <MenuItem onClick={() => navigate("/profile")}>Profile</MenuItem>
+                        */}
+
+                        <MenuItem onClick={handleClose}>Home</MenuItem>
+                        <MenuItem onClick={handleClose}>Past Trips</MenuItem>
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>Settings</MenuItem>
+                    </Menu>
+
+
+                    {/* Logo */}
+                    <img src={Logo} alt="Brand Logo" height={75} />
+
+                    {/* Profile Picture */}
+                    <Avatar 
+                        src={userInfo?.profilePicture}
+                        sx={{ marginLeft: "auto" }}
+                    />
+
+                </Toolbar>
+            </AppBar>
         </Box>
 
 
