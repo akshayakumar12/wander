b3a64b8ae20af232713641359db9d58459ff8ad2
sai-edupulapati <89745999+sai-edupulapati@users.noreply.github.com>
2022-10-21 15:39:26 -0400

    gg

--- my-app/src/frontend/pages/edit profile/editProfile.js
+++ my-app/src/frontend/pages/edit profile/editProfile.js
@@ -1,144 +1,174 @@
-import Header from "../header/header";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import * as React from "react";
-import { DialogContent, DialogContentText, DialogTitle } from "@mui/material";
-import { useState, useEffect } from "react";
-import { auth, db, record, storage } from "../../../firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import Header from '../header/header'
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import * as React from 'react'
+import { DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { useState, useEffect } from 'react'; 
+import {auth, db, record, storage} from "../../../firebase"
+import { deleteUser, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateEmail, updatePassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export default function EditProfile() {
-  const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const [open, setOpen] = React.useState(false);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const navigate = useNavigate();
-  const [user, setUser] = useState("");
-  useEffect(() => {
-    onAuthStateChanged(auth, () => {
-      if (auth.currentUser) {
-        setUser(auth.currentUser);
-      } else {
-        navigate("/");
-      }
-    });
-  }, []);
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const [userInfo, setUserInfo] = useState("");
-  const getData = async () => {
-    const response = db.collection("users");
-    const data = await response.get();
-    data.docs.forEach((item) => {
-      if (item.data().email == auth.currentUser.email) {
-        setUserInfo(item.data());
-      }
-    });
-  };
+    const navigate = useNavigate()
+    const [user, setUser] = useState("")
+    useEffect(() => {
+        onAuthStateChanged(auth, () => {
+            if (auth.currentUser) {
+                setUser(auth.currentUser)
+            } else {
+                navigate('/')
+            }
+        })
+    }, [])
 
-  const modifyData = async (first, last, uname) => {
-    //        db.collections('users').doc(auth.currentUser.email);
-    var userRef = db.collection("users").doc(auth.currentUser.email);
-    userRef.set(
-      {
-        firstName: first,
-        lastName: last,
-        username: uname,
-      },
-      { merge: true }
-    );
-  };
-
-  useEffect(() => {
-    onAuthStateChanged(auth, () => {
-      getData();
-    });
-  }, []);
-
-  // PROFILE PICTURE FUNCTIONALITY
-  const [image, setImage] = useState(null);
-  const [url, setURL] = useState(null);
-
-  const handleImageChange = (e) => {
-    if (e.target.files[0]) {
-      const file = e.target.files[0];
-      var pattern = /image-*/;
-
-      if (!file.type.match(pattern)) {
-        alert("Please choose an image file.");
-        return;
-      }
-
-      setImage(e.target.files[0]);
+    const [userInfo, setUserInfo] = useState("");    
+    const getData = async () => {
+        const response = db.collection('users');
+        const data = await response.get();
+        data.docs.forEach(item=>{
+            if (item.data().email == auth.currentUser.email) {
+                setUserInfo(item.data())
+            }
+        })
     }
-  };
 
-  const handleUpload = () => {
-    const imageRef = ref(storage, auth.currentUser.uid + ".jpg");
-    uploadBytes(imageRef, image)
-      .then(() => {
-        getDownloadURL(imageRef)
-          .then((url) => {
-            setURL(url);
-            db.collection("users").doc(auth.currentUser.email).set(
-              {
-                profilePicture: url,
-              },
-              { merge: true }
-            );
+    const modifyData = async (first, last, uname, email, oldpassword, password) => {
+        var userRef = db.collection('users').doc(email);
+
+        if ((password != userInfo.password) && (password)) {
+            signInWithEmailAndPassword(auth, auth.currentUser.email, oldpassword);
+            String(password);
+            if (password.length < 6) {
+                alert("Weak Password! Please try again!");
+                return;
+            }
+            updatePassword(auth.currentUser, password);
+            userRef.set({
+                password: password
+            }, {merge: true})
+        } else {
+            password = oldpassword;
+        }
+
+        userRef.set({
+            firstName: first,
+            lastName: last,
+            username: uname,
+            email: email,
+        }, {merge: true})
+
+        if (email != auth.currentUser.email) {
+            db.collection('users').doc(auth.currentUser.email).delete();
+            updateEmail(auth.currentUser, email);
+        }
+        
+    }
+
+    const deleteUserFromBase = async (email, pass) => {
+        await signInWithEmailAndPassword(auth, email, pass);
+        await db.collection('users').doc(email).delete();
+        //await signOut(auth);
+        await deleteUser(auth.currentUser);
+        await setOpen(false);
+        navigate('/')
+    }
+    
+    useEffect(()=> {
+        onAuthStateChanged(auth, () => {
+            getData();
+        })
+    }, [])
+
+    const [image, setImage] = useState(null);
+    const [url, setURL] = useState(null);
+
+    const handleImageChange = (e) => {
+        if (e.target.files[0]) {
+            const file = e.target.files[0];
+            var pattern = /image-*/;
+
+            if (!file.type.match(pattern)) {
+                alert("Please choose an image file.");
+                return;
+            }
+
+            setImage(e.target.files[0]);
+        }
+    };
+
+    const handleUpload = () => {
+        const imageRef = ref(storage, auth.currentUser.uid + ".jpg");
+        uploadBytes(imageRef, image)
+          .then(() => {
+            getDownloadURL(imageRef)
+              .then((url) => {
+                setURL(url);
+                db.collection("users").doc(auth.currentUser.email).set(
+                  {
+                    profilePicture: url,
+                  },
+                  { merge: true }
+                );
+              })
+              .catch((error) => {
+                console.log(error.message, "error getting the image url");
+              });
+            setImage(null);
           })
           .catch((error) => {
-            console.log(error.message, "error getting the image url");
+            console.log(error.message);
           });
-        setImage(null);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  };
+      };
+      
 
-  const [first, setFirst] = React.useState("");
-  const [f, setF] = React.useState("abcd");
 
-  const [last, setLast] = React.useState("");
-  const [l, setL] = React.useState("abcd");
+    const [first, setFirst] = React.useState("");
+    const [f, setF] = React.useState("abcd");
+    
+    const [last, setLast] = React.useState("");
+    const [l, setL] = React.useState("abcd");
 
-  const [uname, setUname] = React.useState("");
-  const [u, setU] = React.useState("abcd");
+    const [uname, setUname] = React.useState("");
+    const [u, setU] = React.useState("abcd");
 
-  return (
-    <Box>
-      {/* Header */}
+    const [email, setEmail] = React.useState("");
+    const [e, setE] = React.useState("abcd");
 
-      {/* My Profile Title */}
-      <Stack
-        alignItems={"flex-start"}
-        style={{ marginLeft: "50px", marginRight: "50px" }}
-      >
-        <h1>Edit Profile</h1>
-      </Stack>
+    const [oldpass, setOldpass] = React.useState("");
+    const [newpass, setNewpass] = React.useState("");
 
-      {/* Components Stack */}
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-        style={{ padding: "0px", marginLeft: "50px", marginRight: "50px" }}
-      >
-        {/* Profile Picture*/}
-        <Stack spacing={2} alignItems="center" width="25%">
+    const [deleteuser, setDeleteuser] = React.useState("");
+    const [deletepass, setDeletepass] = React.useState("");
+
+    return (
+
+        <Box>
+            {/* My Profile Title */}
+            <Stack alignItems={"flex-start"} style={{marginLeft: "50px", marginRight: "50px", }}><h1>Edit Profile</h1></Stack>
+
+            {/* Components Stack */}
+            <Stack direction="row" alignItems="center" justifyContent="space-between" style={{padding: "0px", marginLeft: "50px", marginRight: "50px", }}>
+
+                
+                {/* Profile Picture*/}
+                <Stack spacing={2} alignItems="center" width="25%">
           <Avatar
             src={url || userInfo?.profilePicture}
             sx={{ width: 150, height: 150 }}
@@ -154,103 +184,87 @@ export default function EditProfile() {
           </Button>
         </Stack>
 
-        {/* Text Fields */}
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="stretch"
-          spacing={2}
-          width="70%"
-        >
-          <TextField
-            label="First Name"
-            defaultValue="First"
-            value={f ? userInfo.firstName : first}
-            onChange={(event) => {
-              setF("");
-              setFirst(event.target.value);
-            }}
-          />
-          <TextField
-            label="Last Name"
-            defaultValue="Last"
-            value={l ? userInfo.lastName : last}
-            onChange={(event) => {
-              setL("");
-              setLast(event.target.value);
-            }}
-          />
-          <TextField
-            label="Username"
-            defaultValue="username"
-            value={u ? userInfo.username : uname}
-            onChange={(event) => {
-              setU("");
-              setUname(event.target.value);
-            }}
-          />
-          <TextField
-            label="Email Address"
-            defaultValue="name@email.com"
-            value={userInfo.email}
-          />
-          <TextField label="Old Password" type="password" />
-          <TextField label="New Password" type="password" />
-          <TextField label="Confirm Password" type="password" />
+                {/* Text Fields */}
+                <Stack direction="column" justifyContent="center" alignItems="stretch" spacing={2} width="70%">
+                    <TextField
+                        label="First Name"
+                        defaultValue="First"
+                        value={f ? userInfo.firstName : first}
+                        onChange={(event) => { setF(""); setFirst(event.target.value);}}
+                    />
+                    <TextField
+                        label="Last Name"
+                        defaultValue="Last"
+                        value={l ? userInfo.lastName: last}
+                        onChange={(event) => {setL(""); setLast(event.target.value)}}
+                    />
+                    <TextField
+                        label="Username"
+                        defaultValue="username"
+                        value={u ? userInfo.username : uname}
+                        onChange={(event) => {setU(""); setUname(event.target.value)}}
+                    />
+                    <TextField
+                        label="Email Address"
+                        defaultValue="name@email.com"
+                        value={e ? userInfo.email : email}
+                        onChange={(event) => {setE(""); setEmail(event.target.value)}}
+                    />
+                    <TextField
+                        label="Old Password"
+                        type="password"
+                        onChange={(event) => {setOldpass(event.target.value)}}
+                    />
+                    <TextField
+                        label="New Password"
+                        type="password"
+                        onChange={(event) => {setNewpass(event.target.value)}}
+                    />
+                    <TextField
+                        label="Confirm Password"
+                        type="password"
+                    />
 
-          {/* Submit + Delete Buttons */}
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Button
-              variant="contained"
-              onClick={modifyData(
-                first ? first : userInfo.firstName,
-                last ? last : userInfo.lastName,
-                uname ? uname : userInfo.username
-              )}
-            >
-              Submit
-            </Button>
-            <Button variant="contained" color="error" onClick={handleClickOpen}>
-              Delete Account
-            </Button>
-            <Dialog open={open} onClose={handleClose}>
-              <DialogTitle>Confirm Account Action</DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  Are you sure you want to delete your wander account? This
-                  action is permanent and cannot be reverse. If yes, please
-                  reenter your username and password.
-                </DialogContentText>
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="name"
-                  label="Enter Username"
-                  fullWidth
-                  variant="standard"
-                />
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="name"
-                  label="Enter Password"
-                  fullWidth
-                  type="password"
-                  variant="standard"
-                />
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleClose}>Delete Account</Button>
-              </DialogActions>
-            </Dialog>
-          </Stack>
-        </Stack>
-      </Stack>
-    </Box>
-  );
+                    {/* Submit + Delete Buttons */}
+                    <Stack direction="row" alignItems="center" justifyContent="space-between">
+                        <Button variant="contained" onClick={() => {modifyData(first ? first : userInfo.firstName, last ? last : userInfo.lastName, uname ? uname : userInfo.username, email ? email : userInfo.email, oldpass ? oldpass : userInfo.password, newpass ? newpass : "")}}>Submit</Button>
+                        <Button variant="contained" color="error" onClick={handleClickOpen}>Delete Account</Button>
+                        <Dialog open={open} onClose={handleClose}>
+                            <DialogTitle>Confirm Account Action</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    Are you sure you want to delete you wander account? This action is permanent and cannot be reverse.
+                                    If yes, please reenter your email and password.
+                                </DialogContentText>
+                                <TextField 
+                                autoFocus
+                                margin="dense"
+                                id="name"
+                                onChange={(event) => {setDeleteuser(event.target.value)}}
+                                label="Enter Email"
+                                fullWidth
+                                variant="standard"/>
+                                <TextField
+                                autoFocus
+                                margin="dense"
+                                id="name"
+                                onChange={(event) => {setDeletepass(event.target.value)}}
+                                label="Enter Password"
+                                fullWidth
+                                type="password"
+                                variant="standard"/>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button onClick={(event) => {deleteUserFromBase(deleteuser, deletepass)}}>Delete Account</Button>
+                            </DialogActions>
+                        </Dialog>
+
+                    </Stack>
+
+                </Stack>
+            </Stack>
+        </Box>
+
+    )
 }
