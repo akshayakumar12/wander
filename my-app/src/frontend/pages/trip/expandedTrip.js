1a424edcfd9f70137d9ae80ab4491f096f197a79
visathongdee <tthongde@purdue.edu>
2022-11-02 12:18:06 -0400

    ExpTrip: Added a box behind cards to prep for map

--- my-app/src/frontend/pages/trip/expandedTrip.js
+++ my-app/src/frontend/pages/trip/expandedTrip.js
@@ -9,6 +9,7 @@ import { Stack } from "@mui/system";
 import Playlist from "../playlist/playlist";
 import { useNavigate } from "react-router-dom";
 import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
+import Box from "@mui/material/Box";
 
 export default function ExpandedTrip() {
   const navigate = useNavigate();
@@ -18,73 +19,85 @@ export default function ExpandedTrip() {
   };
 
   return (
-    <Stack alignItems={"center"}>
-      <Card
-        sx={{
-          marginTop: "5%",
-          width: "60%",
-          height: "80%",
-          bgcolor: "#F5F7FA",
-          borderRadius: "16px",
-          boxShadow: 3,
-          alignContent: "center",
-        }}
-        disableTouchRipple="true"
-      >
-        <CardActionArea>
-          <CardContent sx={{ marginX: "1%" }}>
-            <Stack direction="row" width="100%">
-              <Stack width="80%">
-                <p
-                  align="Left"
-                  style={{
-                    fontSize: "20px",
-                    marginBottom: 20,
-                    fontWeight: "bold",
-                  }}
-                >
-                  Source Location
-                </p>
-                <p
-                  align="Left"
-                  style={{
-                    fontSize: "20px",
-                    marginTop: 30,
-                    margin: 0,
-                    fontWeight: "bold",
-                  }}
-                >
-                  Destination
-                </p>
-              </Stack>
-              <Stack width="10%">
-                <Button variant="contained" onClick={editTrip_click}>
-                  Edit
-                </Button>
-              </Stack>
-            </Stack>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card
-        sx={{
-          marginTop: "5%",
-          width: "60%",
-          height: "80%",
-          bgcolor: "#F5F7FA",
-          borderRadius: "16px",
-          boxShadow: 3,
-          alignContent: "center",
-        }}
-        disableTouchRipple="true"
-      >
-        <CardActionArea
-          onClick={() => navigate("../playlist")}
-          sx={{ paddingBottom: "2%" }}
+    <>
+      <Box
+        position="absolute"
+        zIndex={0}
+        width="100%"
+        height="1000px"
+        color="gray"
+        sx={{ backgroundColor: "gray" }}
+      ></Box>
+
+      {/* front stack */}
+      <Stack position="absolute" paddingLeft="5%" width="60%" zIndex={1}>
+        <Card
+          sx={{
+            marginTop: "5%",
+            width: "60%",
+            height: "80%",
+            bgcolor: "#F5F7FA",
+            borderRadius: "16px",
+            boxShadow: 3,
+            alignContent: "center",
+          }}
+          disableTouchRipple="true"
         >
-          <Playlist></Playlist>
-        </CardActionArea>
-      </Card>
-    </Stack>
+          <CardActionArea>
+            <CardContent sx={{ marginX: "1%" }}>
+              <Stack direction="row" width="100%">
+                <Stack width="80%">
+                  <p
+                    align="Left"
+                    style={{
+                      fontSize: "20px",
+                      marginBottom: 20,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Source Location
+                  </p>
+                  <p
+                    align="Left"
+                    style={{
+                      fontSize: "20px",
+                      marginTop: 30,
+                      margin: 0,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Destination
+                  </p>
+                </Stack>
+                <Stack width="10%">
+                  <Button variant="contained" onClick={editTrip_click}>
+                    Edit
+                  </Button>
+                </Stack>
+              </Stack>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card
+          sx={{
+            marginTop: "5%",
+            width: "60%",
+            height: "80%",
+            bgcolor: "#F5F7FA",
+            borderRadius: "16px",
+            boxShadow: 3,
+            alignContent: "center",
+          }}
+          disableTouchRipple="true"
+        >
+          <CardActionArea
+            onClick={() => navigate("../playlist")}
+            sx={{ paddingBottom: "2%" }}
+          >
+            <Playlist></Playlist>
+          </CardActionArea>
+        </Card>
+      </Stack>
+    </>
   );
 }
