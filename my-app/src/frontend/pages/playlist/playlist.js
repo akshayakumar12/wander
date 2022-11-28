396038ffd99c17f6f77159f26d13fd788c54df60
goodvid <vuppalav@purdue.edu>
2022-11-27 23:05:22 -0500

    send email and update dynamic playlist nav

--- my-app/src/frontend/pages/playlist/playlist.js
+++ my-app/src/frontend/pages/playlist/playlist.js
@@ -4,7 +4,7 @@ function Playlist(props) {
   const location = useLocation();
   const link =
     location.state === null
-      ? "https://open.spotify.com/embed/playlist/4WD1BEKXBaXT7NwXa6RNfU?si=d7baa3d91bcb4429?utm_source=generator"
+      ? "https://open.spotify.com/embed/playlist/4WD1BEKXBaXT7NwXa6RNfU?si=d7baa3d91bcb4429?utm_source=generator" //better way of setting default (maybe have current object)
       : location.state.Playlist;
   //console.log(location);
   return (
