f5715e9733c244e0d6519cefe8c93fe4351e221e
visathongdee <tthongde@purdue.edu>
2022-11-29 15:57:52 -0500

    Theme: form label color override + attempted custom textfield

--- my-app/src/frontend/pages/theme.js
+++ my-app/src/frontend/pages/theme.js
@@ -1,4 +1,7 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { alpha, styled } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import "./style.css";
 
 export const Theme = createTheme({
   palette: {
@@ -14,4 +17,56 @@ export const Theme = createTheme({
       main: "#4caf50",
     },
   },
+  typography: {
+    fontFamily: "Rubik",
+  },
+  components: {
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          "&.Mui-focused": {
+            // increase the specificity for the pseudo class
+            color: "#666666",
+          },
+        },
+      },
+    },
+  },
 });
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(2),
+    padding: "3",
+  },
+  "& .MuiInputBase-input": {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor:
+      theme.palette.mode === "light" ? "primary.contrastText" : "#2b2b2b",
+    border: "1px solid #c5c5c4",
+    fontSize: 16,
+    width: "100%",
+    padding: "10px 12px",
+    transition: theme.transitions.create(["border-color", "background-color"]),
+    fontFamily: ["Rubik"].join(","),
+    "&:focus": {
+      /*
+      boxShadow: `${alpha(
+        theme.palette.primary.contrastText,
+        0.25
+      )} 0 0 0 0.2rem`,
+      */
+      borderColor: "#DE6600",
+      borderWidth: 2,
+    },
+  },
+}));
+
+export default function BootstrapUsage() {
+  return (
+    <ThemeProvider theme={Theme}>
+      <BootstrapInput />
+    </ThemeProvider>
+  );
+}
