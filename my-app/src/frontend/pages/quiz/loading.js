e8f83de9a0279bbf06543233f5f4d816c85c0935
visathongdee <tthongde@purdue.edu>
2022-12-02 02:01:54 -0500

    fixed loading page not showing

--- my-app/src/frontend/pages/quiz/loading.js
+++ my-app/src/frontend/pages/quiz/loading.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactLoading from "react-loading";
+import { useEffect, useState } from "react";
 
 export default function Loading() {
+  const [bgColor, setBgColor] = useState("");
+  useEffect(() => {
+    console.log(localStorage.getItem("theme"));
+    const value = localStorage.getItem("theme");
+    console.log("type: " + typeof value);
+    console.log("value: " + value);
+    value == "true" ? setBgColor("#F5ECE3") : setBgColor("#02407F");
+  });
   return (
     <div
       style={{
@@ -11,12 +20,9 @@ export default function Loading() {
         transform: "translate(-50%, -50%)",
       }}
     >
-      <ReactLoading
-        type="bubbles"
-        sx={{ color: "primary.contrastText" }}
-        height={300}
-        width={150}
-      />
+      {/*
+       */}
+      <ReactLoading type="bubbles" color={bgColor} height={150} width={150} />
     </div>
   );
 }
